8c14010514feeb1d17524cb38326114f30628510
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-06-06 21:50:34 +0700

    Add apply pruning when specified

--- video_compression/setup/train_test.py
+++ video_compression/setup/train_test.py
@@ -4,6 +4,7 @@ from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint, Mo
 
 from typing import Any, Dict, Optional, Union
 import os
+from copy import copy
 from pathlib import Path
 PRJ_ROOT = Path(__file__).parent.parent.parent.resolve()
 
@@ -29,16 +30,24 @@ def train(
         drop_last=False,
     )
 ):  
-    prune_at_epochs = [int(i * trainer_params["max_epochs"]) for i in compress_params["prune_at_epochs"]]
+    prune_at_epochs = sorted([int(i * trainer_params["max_epochs"]) for i in compress_params["prune_at_epochs"]])
+    
     def compute_amount(epoch):
+        prune_at_epochs = copy(prune_at_epochs)
         if len(prune_at_epochs) == 1:
             if epoch == prune_at_epochs[0]:
                 return compress_params["prune_ratio"]
-        else:
-            base_prune_ratio = compress_params["prune_ratio"] ** (1 / len(prune_at_epochs))
-            for i in range(len(prune_at_epochs)-1):
-                if prune_at_epochs[i] <= epoch < prune_at_epochs[i+1]:
-                    return base_prune_ratio ** (i+2)
+            return 0
+        base_prune_ratio = compress_params["prune_ratio"] ** (1 / len(prune_at_epochs))
+        for i in range(len(prune_at_epochs)-1):
+            if prune_at_epochs[i] <= epoch < prune_at_epochs[i+1]:
+                return base_prune_ratio ** (i+2)
+        return 0
+    
+    def prune_when(epoch):
+        if epoch in prune_at_epochs:
+            return True
+        return False
 
     trainer = L.Trainer(
         accelerator="auto",
@@ -50,7 +59,8 @@ def train(
                 parameters_to_prune=compress_params["parameters_to_prune"],
                 pruning_fn="l1_unstructured",
                 amount=compute_amount,
-                use_global_unstructured=True
+                use_global_unstructured=True,
+                apply_pruning=prune_when,
             )
         ],
         **trainer_params,
